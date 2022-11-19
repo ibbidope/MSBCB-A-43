c68f1d0d3d2700f9371271d916cde0a5d92fb22a
ibbidope <ibbidope@gmail.com>
2022-11-19 13:07:03 +0500

    modified:   Class Work/11-19-2022/my-app/src/components/counter.jsx

--- Class Work/11-19-2022/my-app/src/components/counter.jsx
+++ Class Work/11-19-2022/my-app/src/components/counter.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-  const handler = () => {
+
+  const incrmnt = () => {
     setCount(count + 1);
+  };
+  const dccrmnt = () => {
+    setCount(count - 1);
   };
   return (
     <div>
       <span>{count}</span>
-      <button onClick={handler}>Increment</button>
+      <button onClick={incrmnt}>Increment</button>
+      <button onClick={dccrmnt}>Decrement</button>
     </div>
   );
 };
