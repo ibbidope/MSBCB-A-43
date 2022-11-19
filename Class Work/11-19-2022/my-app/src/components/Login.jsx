d66d1da3703e18e63ab9787c52644835542b468f
ibbidope <ibbidope@gmail.com>
2022-11-19 12:54:28 +0500

    modified:   Class Work/11-19-2022/my-app/src/components/Login.jsx 	modified:   Class Work/11-19-2022/my-app/src/components/counter.jsx

--- Class Work/11-19-2022/my-app/src/components/Login.jsx
+++ Class Work/11-19-2022/my-app/src/components/Login.jsx
@@ -24,26 +24,26 @@ import React, { useRef, useState } from "react";
 
 // Controlled
 export default function LoginForm() {
-    const [nameField, setNameField] = useState("");
-    const handleSubmit = (event) => {};
-    const handleChange = (event) => {
-        console.log("event.target.value : ", event.target.name);
-        if(event.target.name === "nameField"){
-            //check name
-        }
-        else{}
-        setNameField(event.target.value);
-    };
-    return (
-      <div>
-        <h1>Login Form</h1>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="username">Username</label>
-          <input type="text" ref={nameField} />
-          <label htmlFor="password">Password</label>
-          <input type="password" />
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  }
+  const [nameField, setNameField] = useState("");
+  const handleSubmit = (event) => {};
+  const handleChange = (event) => {
+    console.log("event.target.value : ", event.target.name);
+    if (event.target.name === "nameField") {
+      //check name
+    } else {
+    }
+    setNameField(event.target.value);
+  };
+  return (
+    <div>
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input type="text" ref={nameField} />
+        <label htmlFor="password">Password</label>
+        <input type="password" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
--- Class Work/11-19-2022/my-app/src/components/counter.jsx
+++ Class Work/11-19-2022/my-app/src/components/counter.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
-    const handler = () => {
-        setCount(count+1);
-    }
-    return (
-        <div>
-            <span>{count}</span>
-            <button onClick={handler}>Increment</button>
-        </div>
-    );
-}
- 
+  const [count, setCount] = useState(0);
+  const handler = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <span>{count}</span>
+      <button onClick={handler}>Increment</button>
+    </div>
+  );
+};
+
 export default Counter;
